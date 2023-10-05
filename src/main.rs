acb3adc0d89bdb6522fd9a98f8246cc306c1e7af
WiseMrMusa <abdulkareemusa01@gmail.com>
2023-10-05 23:52:44 +0100

    clean code

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
 use ecc_naive::field_point::field_point;
-use field_point::{Point, ECC, PointTrait};
+use field_point::{ ECC, PointTrait};
 
 fn main() {
-    // let p1 = Point::new(&2, &5, &5, &7);
-    // let p2 = Point::new(&2, &5, &5, &7);
-    // let _p3 = Point::new(&-1, &-1, &5, &7);
-    // println!("{}", p1 + p2)
 
     let ep = ECC::new(&5, &7);
     let _ep1 = ep.eleptic_point(&2, &5);
